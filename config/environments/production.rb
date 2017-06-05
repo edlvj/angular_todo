18e9635f457613ffd332c950749170e530a22cce
Ed Gorach <gorachedik96@gmail.com>
2017-06-05 08:36:38 +0000

    try configurate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.assets.compile = false
+  config.serve_static_assets = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
